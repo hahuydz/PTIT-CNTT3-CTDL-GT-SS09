4c63e0785d0e44271cc35b3ed12f519e4be6d0f4
hahuydz <haquanghuy2006abc@gmail.com>
2025-07-02 23:58:58 +0700

    b2.c

---
+++ b2.c
@@ -0,0 +1,46 @@
+//
+// Created by admin on 7/2/2025.
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* next ;
+}node;
+
+node* createNode (int data) {
+    node* newnode = (node*) malloc(sizeof(node));
+    newnode ->data = data;
+    newnode ->next = NULL;
+    return newnode;
+}
+void printNode (node* head) {
+    node* curent = head;
+    while (curent!= NULL) {
+        printf("%d->",curent->data);
+        curent=curent->next;
+    }
+    printf("NULL");
+}
+int main () {
+    int n;
+    printf("nhap so luong node muon tao");
+    scanf("%d",&n);
+    int arr[n];
+    printf("nhap gia tri cua cac node \n");
+    for (int i = 0; i < n; ++i) {
+        scanf("%d",&arr[i]);
+    }
+    node* head = createNode(arr[0]);
+    node* curent = head;
+    for (int i = 1; i < n; ++i) {
+        node* newnode = createNode(arr[i]);
+        curent->next = newnode;
+        curent = newnode;
+    }
+    printNode(head);
+
+
+    return 0;
+}
